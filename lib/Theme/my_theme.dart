2fb34f14e78a3a2244f274a9c4574dcdafce839f
s <51485083+sriku00@users.noreply.github.com>
2021-03-07 10:29:10 +0530

    day 1

---
+++ lib/Theme/my_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  ThemeData _themeData = ThemeData();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: "Supreme",
       home: Scaffold(
         appBar: AppBar(
           elevation: 0.0,
