88c3cd505f22e8f1d1c52465c27a42d0d66a41d3
s <51485083+sriku00@users.noreply.github.com>
2021-03-08 16:40:47 +0530

    error solved

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      // ignore: prefer_single_quotes
+      body: Image.asset("assets/images/user_2.png"),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:chat_app/Screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Supreme",
-      home: Tex("Home Page"),
-    );
+        title: 'Supreme chat',
+        // ignore: prefer_single_quotes
+        home: AnimatedSplashScreen(splash: "assets/images/user_2.png", nextScreen: HomeScreen()));
   }
 }
